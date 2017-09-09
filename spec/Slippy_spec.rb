64ce227259de3e9178a1ca4092a9b9d22a50c1f1
StephenMcGuckinDAT <stephendmcguckin@gmail.com>
2017-09-08 20:50:02 -0700

    Revert "Fixed gemspec"
    This reverts commit e0c0ad0f9f05c5a1a3cde38488d9af12efed5cdb.

--- spec/Slippy_spec.rb
+++ spec/Slippy_spec.rb
@@ -1,8 +1,11 @@
-
-require 'spec_helper'
+require "spec_helper"
 
 RSpec.describe Slippy do
-  it 'has a version number' do
+  it "has a version number" do
     expect(Slippy::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
   end
 end
