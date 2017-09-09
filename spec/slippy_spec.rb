5d6ff929954f1293599c604d01267db628bf88da
StephenMcGuckinDAT <stephendmcguckin@gmail.com>
2017-09-08 20:53:34 -0700

    removed unit test

--- spec/slippy_spec.rb
+++ spec/slippy_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Slippy do
   it 'has a version number' do
     expect(Slippy::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
